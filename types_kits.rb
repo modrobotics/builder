7208f057653fce2a780d5ea9e98703cbf24d5475
Sawyer Bernath <sawyerbernath@gmail.com>
2016-02-09 14:39:51 -0700

    Add huck tank submissions

--- types_kits.rb
+++ types_kits.rb
@@ -4,10 +4,12 @@ box_cubelet = JSON.parse(HTTParty.get("http://tasks.internal.modrobotics.com/tas
 malc = JSON.parse(HTTParty.get("http://tasks.internal.modrobotics.com/task_types?tag=malc", headers: {'accept'=>"application/json"}).body)
 final_moss = JSON.parse(HTTParty.get("http://tasks.internal.modrobotics.com/task_types?tag=final_moss", headers: {'accept'=>"application/json"}).body)
 kitting = JSON.parse(HTTParty.get("http://tasks.internal.modrobotics.com/task_types?tag=kitting", headers: {'accept'=>"application/json"}).body)
+huck = JSON.parse(HTTParty.get("http://tasks.internal.modrobotics.com/task_types?tag=huck", headers: {'accept'=>"application/json"}).body)
 
 TYPES = [
          {title: 'Box Cubelets', types: box_cubelet}, 
          {title: 'Box MOSS Units', types: malc}, 
          {title: 'MOSS Non-Boxed Submission', types: final_moss}, 
-         {title: 'Kits', types: kitting}
+         {title: 'Kits', types: kitting},
+         {title: 'Huck Tank', types: huck}
         ]
